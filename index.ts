cda6c7aa223d9c5a600bde7031097b15368050f1
Nicolas Wüstner <nicolas.wuestner@baral-geohaus.de>
2024-07-10 13:33:59 +0200

    fixes and basic server

--- index.ts
+++ index.ts
@@ -1,1 +1,8 @@
-console.log("Hello via Bun!");
+const server = Bun.serve({
+    port: Bun.env.PORT || 8000,
+    fetch() {
+        return new Response('Hello via Bun!');
+    },
+});
+
+console.log(`Listening on port ${server.port}...`);
